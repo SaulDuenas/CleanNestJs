5e12160390133df59f746ce84d58dd641d1ea30e
SaulDuenas <saul.duenas@hotmail.com>
2024-10-28 18:29:45 -0600

    template conf

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './Modules/app.module';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+   );
+
+  const options = new DocumentBuilder()
+  .setTitle('CleanNestJs')
+  .setDescription('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app,options);
+
+  SwaggerModule.setup('api/docs',app,document);
+
+  await app.listen(envs.port);
+  console.log(`App runing on port ${ envs.port}`);
 }
 bootstrap();
